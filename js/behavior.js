46967db64e9368d44910bb6388afc7e63247cfc7
Nicole Seo <nicseo@mit.edu>
2016-04-29 17:36:23 -0400

    Cuisine panel reactivity

--- js/behavior.js
+++ js/behavior.js
@@ -1,5 +1,23 @@
+var cuisineTypes = ['Chinese','Vietnamese','Japanese','Korean','Thai','American','Southern',
+					'Barbecue','Soul Food','Cajun','Jamaican','Irish','Scottish','English',
+					'Diner Food','Pub Food','Americana','Continental','African'];
+
 var main = function() {
-	console.log("Loaded");
+
+	for (var i=0; i<cuisineTypes.length; i++) {
+		$('.cuisine-panel').append('<a href="#"><p>'+cuisineTypes[i]+'</p></a>');
+	}
+
+
+	$('.cuisine-panel > a').click(function() {
+		console.log("CUISINE CHOSEN");
+		if (!$(this).hasClass('active')) {
+			$(this).addClass('active');
+		} else {
+			$(this).removeClass('active');
+		}
+		
+	});
 }
 
 $(document).ready(main());
